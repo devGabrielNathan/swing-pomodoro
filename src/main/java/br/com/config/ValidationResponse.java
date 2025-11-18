6a31dba27f3cfc873d73b8587cec938df0028a03
Gabriel Nathan dos Santos Pires <devgabrielnathan@gmail.com>
2025-11-17 22:56:35 -0300

    adding config to exceptions

---
+++ src/main/java/br/com/config/ValidationResponse.java
@@ -0,0 +1,34 @@
+package br.com.config;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ValidationResponse {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+    private HashMap<String, String> errors;
+
+    public ValidationResponse() {}
+
+    public ValidationResponse(LocalDateTime timestamp, HashMap<String, String> errors) {
+        this.timestamp = timestamp;
+        this.errors = errors;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public HashMap<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(HashMap<String, String> errors) {
+        this.errors = errors;
+    }
+}
